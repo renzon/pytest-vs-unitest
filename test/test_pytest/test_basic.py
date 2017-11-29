4b62147d5b11038cb30e847d175534f570dd550a
renzon <renzon@gmail.com>
2017-11-28 22:42:50 -0200

    Step 4: Failure messages

--- test/test_pytest/test_basic.py
+++ test/test_pytest/test_basic.py
@@ -1,2 +1,2 @@
 def test_basic_assertion():
-    assert True
+    assert False
--- test/test_unitest/test_basic.py
+++ test/test_unitest/test_basic.py
@@ -3,4 +3,4 @@ from unittest.case import TestCase
 
 class BasicAssertionTestCase(TestCase):
     def test_basic_assertion(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
